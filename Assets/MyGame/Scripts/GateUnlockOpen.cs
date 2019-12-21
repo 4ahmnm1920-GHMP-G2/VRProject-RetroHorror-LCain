c1fa68ae4633651319e9f04bb0f6d16da82cc0e6
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2019-12-21 14:27:12 +0100

    Added key, lock mesh, animations

---
+++ Assets/MyGame/Scripts/GateUnlockOpen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateUnlockOpen : MonoBehaviour
+{
+    public float RotationDirection = 70.0f;
+
+    bool isRunning = false;
+    public float waitTime;
+
+    // Look for UpArrow key
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isRunning = true;
+        }
+
+        
+        if (isRunning)
+        {
+            waitTime += Time.deltaTime;
+            
+            if (waitTime <= 1)
+            {
+                Rotate(RotationDirection);
+                
+            }
+            else {
+                isRunning = false;
+                waitTime = 0;
+            }
+        }
+        
+    }
+
+    //Rotate the Gates
+    private void Rotate(float RotationDirection)
+    {
+        transform.Rotate(0.0f, 0.0f, RotationDirection * Time.deltaTime, Space.Self);
+        return;
+    }
+}
