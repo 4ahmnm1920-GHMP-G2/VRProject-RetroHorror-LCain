8ef9e0e99e8861fc787017e9a3d89df9fc0e1559
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2019-12-29 02:32:33 +0100

    added fire fx, spots eyes, itween, clock prop

---
+++ Assets/MyGame/Models/Characters/Spots/EyeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeScript : MonoBehaviour
+{
+    float eyetimer;
+    private MeshRenderer myRender;
+
+    void Start()
+    {
+        myRender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eyetimer = Random.Range(0.0f, 20.0f);
+        if(eyetimer <= 0.5f)
+        {
+            myRender.enabled = true;
+        }
+        else if(eyetimer >= 17.5f)
+        {
+            myRender.enabled = false;
+        }
+    }
+}
