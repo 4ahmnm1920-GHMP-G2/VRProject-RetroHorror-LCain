674cae0d652452e7ba21fa19c943174297242e44
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2020-03-03 15:15:55 +0100

    included test circular drive

---
+++ Assets/MyGame/Scripts/DoorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TeleportTo;
+    public bool isTeleporting;
+
+    private void Start()
+    {
+        Player = GameObject.Find("TeleportGroup");
+    }
+
+    public void TeleportMe()
+    {
+        Player.transform.position = TeleportTo.transform.position;
+    }
+}
