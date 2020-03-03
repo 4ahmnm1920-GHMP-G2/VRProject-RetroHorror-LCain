0fd226494a9a48c37f7194133be4577202c7859a
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2020-03-03 16:18:04 +0100

    updated scene to work with new door scripts

--- Assets/MyGame/Scripts/DoorScript.cs
+++ Assets/MyGame/Scripts/DoorScript.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject PlayerBody;
     public GameObject TeleportTo;
     public bool isTeleporting;
+    private Quaternion ThisRot;
 
     private void Start()
     {
         Player = GameObject.Find("TeleportGroup");
+        PlayerBody = Player.transform.GetChild(0).gameObject;
+        ThisRot = this.gameObject.transform.rotation;
     }
 
     public void TeleportMe()
     {
         Player.transform.position = TeleportTo.transform.position;
+        PlayerBody.transform.position = TeleportTo.transform.position;
+        this.gameObject.transform.rotation = ThisRot;
     }
 }
